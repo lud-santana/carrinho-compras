1813419ba44e5347acca698decfd0538f8c173c0
lud-santana <yurisilvasantana2011@hotmail.com>
2024-02-05 08:00:31 -0300

    Primeiro commit, introduzir funções

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function adicionar()
+{
+    /* Recuperar valores como:
+    - Nome do Produto;
+    - Quantidade; e
+    - Valor
+
+    Cálculo do preço (subtotal)
+
+    Adicionar ao carrinho
+
+    Atualizar o valor total
+*/
+    nomeProduto = document.getElementById('produto');
+    quantidade = document.getElementById('quantidade');
+
+    
+    
+}
+
+function limpar()
+{
+
+}
