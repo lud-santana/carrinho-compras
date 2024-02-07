815dd4a36fd7e6860be7c90e889ab8168dd7f803
lud-santana <yurisilvasantana2011@hotmail.com>
2024-02-07 07:42:08 -0300

    Implementar a função Adicionar, a função Limpar e dinamizar o site

--- js/app.js
+++ js/app.js
@@ -1,24 +1,44 @@
+//Declarando a variável para posterior cálculo do valor total do carrinho
+let valorFinal;
+//Limpando os campos ao iniciar o HTML
+limpar();
+
 function adicionar()
 {
     /* Recuperar valores como:
     - Nome do Produto;
     - Quantidade; e
-    - Valor
+    - Valor  */
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
 
-    Cálculo do preço (subtotal)
+    //Cálculo do preço (subtotal)
+    let preco = quantidade * valorUnitario;
 
-    Adicionar ao carrinho
+    //Adicionar ao carrinho
+    let carrinho = document.getElementById('lista-produtos');
 
-    Atualizar o valor total
-*/
-    nomeProduto = document.getElementById('produto');
-    quantidade = document.getElementById('quantidade');
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`
 
-    
-    
+    //Atualizar o valor total
+    valorFinal = valorFinal + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${valorFinal}`;
+
+    //Limpando o campo quantidade ao adicionar um produto
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar()
 {
+    //Limpeza dos campos estruturados na página
+
+    valorFinal = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 
 }
